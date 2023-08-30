4f46e8a3284b2c079df0cbe86c46be6c51855e6b
Nipun Shaharavat <nipunsherawat@gmail.com>
2023-08-30 14:02:38 +0530

    assignment2

---
+++ palindromeproblem.js
@@ -0,0 +1,13 @@
+function palindrome(str)
+{
+let bag=[]
+let limit=1000;
+for(var i=0; i<limit; i++){
+    if(x%y==0){
+        consolee.log("mmangu")
+    }
+}
+if(mangu==z){
+    console.log(zz)
+}
+}
